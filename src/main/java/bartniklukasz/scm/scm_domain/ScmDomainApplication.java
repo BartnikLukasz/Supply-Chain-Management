5316e656f13dec84778d1961fdcb83e5e13acf03
lbartnik <lukasz.bartnik@enigma.com.pl>
2022-12-20 16:15:09 +0100

    Init

---
+++ src/main/java/bartniklukasz/scm/scm_domain/ScmDomainApplication.java
@@ -0,0 +1,13 @@
+package bartniklukasz.scm.scm_domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScmDomainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bartniklukasz/scm/scm_domain/ScmDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package bartniklukasz.scm.scm_domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
